e901ab652b8ac59c45450c2b99565e916c2599e6
Aditya Kumar <29352643+kumar306@users.noreply.github.com>
2023-03-08 00:37:07 +0530

    setup ganache, truffle, sample contract

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,7 @@
+// const { artifacts } = require("truffle");
+// eslint-disable-next-line no-undef
+const Migrations = artifacts.require("Migrations");
+
+module.exports = function (deployer) {
+  deployer.deploy(Migrations);
+};
